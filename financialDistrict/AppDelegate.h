7f426a7f2333a9ebbc2c7a93b4f80d07abf2ca61
yiplee <guoyinl@gmail.com>
2013-03-11 10:24:29 +0800

    Initial Commit

---
+++ financialDistrict/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  financialDistrict
+//
+//  Created by USTB on 13-3-11.
+//  Copyright (c) 2013年 USTB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ financialDistrict/SecondViewController.m
@@ -0,0 +1,29 @@
+//
+//  SecondViewController.m
+//  financialDistrict
+//
+//  Created by USTB on 13-3-11.
+//  Copyright (c) 2013年 USTB. All rights reserved.
+//
+
+#import "SecondViewController.h"
+
+@interface SecondViewController ()
+
+@end
+
+@implementation SecondViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
