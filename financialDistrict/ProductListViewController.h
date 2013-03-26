32dd83da7ab134e0fa6bbb214c453d1aad79219b
yiplee <guoyinl@gmail.com>
2013-03-26 11:48:59 +0800

    Improved UI features

---
+++ financialDistrict/ProductListViewController.h
@@ -0,0 +1,16 @@
+//
+//  ProductListViewController.h
+//  jrj
+//
+//  Created by jrj on 13-3-25.
+//  Copyright (c) 2013年 jrj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ProductListViewController : UITableViewController
+
+@property NSMutableArray *data;
+
+
+@end
